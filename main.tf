bbf194bb1d755eda75d5602f7f9e79edd440ef6f
Ishita Kapoor <ishita1904kapoor@gmail.com>
2026-04-23 14:03:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "key" {
+  source = "./modules/resources/key"
+
+  description                        = var.description
+  key_usage                          = var.key_usage
+  custom_key_store_id                = var.custom_key_store_id
+  customer_master_key_spec           = var.customer_master_key_spec
+  deletion_window_in_days            = var.deletion_window_in_days
+  is_enabled                         = var.is_enabled
+  enable_key_rotation                = var.enable_key_rotation
+  rotation_period_in_days            = var.rotation_period_in_days
+  multi_region                       = var.multi_region
+  tags                               = var.tags
+  xks_key_id                         = var.xks_key_id
+}
