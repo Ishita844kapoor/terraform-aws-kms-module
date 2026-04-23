505d93afa9ba24171565212b2dee84f6a0b16e7f
Ishita Kapoor <ishita1904kapoor@gmail.com>
2026-04-23 14:20:32 +0530

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -5,6 +5,8 @@ module "key" {
   key_usage                          = var.key_usage
   custom_key_store_id                = var.custom_key_store_id
   customer_master_key_spec           = var.customer_master_key_spec
+  policy                             = var.policy
+  bypass_policy_lockout_safety_check = var.bypass_policy_lockout_safety_check
   deletion_window_in_days            = var.deletion_window_in_days
   is_enabled                         = var.is_enabled
   enable_key_rotation                = var.enable_key_rotation
