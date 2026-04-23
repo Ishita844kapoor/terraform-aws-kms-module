b58933bc119bff5d269f5e685471cdda22bd3d29
Ishita Kapoor <ishita1904kapoor@gmail.com>
2026-04-23 14:06:46 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "key_id" {
+  description = "ID of the created KMS key"
+  value       = module.key.key_id
+}
+
+output "arn" {
+  description = "ARN of the created KMS key"
+  value       = module.key.arn
+}
+
+output "tags" {
+  description = "ARN of the created KMS key"
+  value       = module.key.tags_all
+}
