abc6d839e398e81f1cf15d7c172f27704e578fab
Ishita Kapoor <ishita1904kapoor@gmail.com>
2026-04-29 12:55:23 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,17 @@ output "tags" {
   description = "ARN of the created KMS key"
   value       = module.key.tags_all
 }
+
+############################
+# KMS Alias Outputs
+############################
+output "kms_alias_arn" {
+  description = "ARN of the created KMS alias"
+  value       = module.alias.arn
+}
+
+output "kms_alias_target_key_arn" {
+  description = "Target key ARN associated with the alias"
+  value       = module.alias.target_key_arn
+}
+
