f6a1c687e69801838dae3dd0bd603bd11bc561ba
Ishita Kapoor <ishita1904kapoor@gmail.com>
2026-04-29 13:24:30 +0530

    Delete modules/resources/alias directory

--- modules/resources/alias/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_kms_alias" "default" {
-  name           = var.name
-  target_key_id  = var.target_key_id                                      
-}
--- modules/resources/alias/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "This output variable returns the Amazon Resource Name (ARN) of the key alias."
-  value       = aws_kms_alias.default.arn
-}
-
-output "target_key_arn" {
-  description = "This output variable returns the Amazon Resource Name (ARN) of the target key identifier."
-  value       = aws_kms_alias.default.target_key_arn
-}
